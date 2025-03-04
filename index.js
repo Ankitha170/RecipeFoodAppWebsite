23f798c7638a8b2638896e917d64a2106d6c5298
Ankitha170 <lakavathankitha524@gmail.com>
2025-03-04 20:12:09 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-export * from "./AbortController";
-export * from "./AbortSignal";
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import { userRouter } from "./routes/user.js";
+import { recipesRouter } from "./routes/recipes.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/auth", userRouter);
+app.use("/recipes", recipesRouter);
+
+mongoose.connect(
+  "mongodb+srv://ankitha123:ankitha123@cluster0.bwegp.mongodb.net/recipetest?retryWrites=true&w=majority&appName=Cluster0",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then(
+    console.log("DB connected...")
+  )
+
+app.listen(3001, () => console.log("Server started"));
